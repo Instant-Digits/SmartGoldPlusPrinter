3e009d90a3ab1f1e77e9726db036c3b41afc2823
shagananthiru <shaganan@gmail.com>
2022-05-10 00:26:43 +0800

    artisdam

--- stockPrinter.py
+++ stockPrinter.py
@@ -19,7 +19,7 @@ def SetPrintingJobStock(printData):
 
     y=800
     l =0
-    lSpace=15
+    lSpace=13
     page =1
 
 
@@ -55,12 +55,13 @@ def SetPrintingJobStock(printData):
                     l+=1
 
 
-
+                
                 if (type(value2) is dict):
                     can.setFont("Helvetica-Bold", 11)
 
                     can.drawString(80, y-l*lSpace, nameDecode(key2) +'  : '+ currencyFormater(value2['qts'])+'  || W : '+ currencyFormater(value2['weight'])+'g')        
                     l+=1
+                    
                     for (key3, value3) in value2.items():
                         
                         if (y-l*lSpace-lSpace<0):
@@ -81,6 +82,37 @@ def SetPrintingJobStock(printData):
                             can.drawString(100, y-l*lSpace, nameDecode(key3))
                             can.drawString(340, y-l*lSpace, ': '+ currencyFormater(value3['qts'])+'  || W : '+ currencyFormater(value3['weight'])+'g')        
                             l+=1
+
+                            if (value3['ids']):
+                                can.setFont("Helvetica", 10)
+                                id=0
+                                while (id< len(value3['ids'])):
+                                    if (y-l*lSpace-2*lSpace<0):
+                                        can.showPage()
+                                        y=800
+                                        l =0
+                                        page+=1
+                                        can.drawString(60, y-l*lSpace,'Page No : '+str(page)+'    Type : '+typeInv+ "      Printed on : "+ printData['printedOn'])
+                                        l+=1
+                                        l+=1
+                                        can.setFont("Helvetica-Bold", 12)
+                                        can.drawString(100, y-l*lSpace, 'Continued..')
+                                        l+=1
+                                    k=0
+                                    while (k<4): 
+                                        if(id< len(value3['ids']) and value3['ids'][id]):
+                                            can.setFont("Helvetica", 10)
+                                            itemDet = value3['ids'][id].split('@')
+                                            can.drawString(110+ k*120, y-l*lSpace, itemDet[1]+' - '+itemDet[0]+'g')
+                                            k+=1
+                                        elif ( id> len(value3['ids'])):
+                                            k=5 
+                                        id+=1
+                                    l+=1
+                                l+=1
+
+                                    
+
 
 
     can.save()
